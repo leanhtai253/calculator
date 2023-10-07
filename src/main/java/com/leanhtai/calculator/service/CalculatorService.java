aec444e382f020d4675dc5eb8ab016b4aaf7a8a6
Tai Le <leanhtai253@gmail.com>
2023-10-07 21:03:24 +0700

    Added checkstyle

--- src/main/java/com/leanhtai/calculator/service/CalculatorService.java
+++ src/main/java/com/leanhtai/calculator/service/CalculatorService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+    private static final double PI = 3.14;
     public int sum(int a, int b) {
         return a + b;
     }
